3c1e0d79c95aae538df2eb8c996b78628288bfd3
Alejandro Kolton <47402803+droyktton@users.noreply.github.com>
2026-05-03 18:23:49 -0300

    Comment out C_lo and C_hi initialization
    Commented out the initialization of C_lo and C_hi variables.

--- main.cu
+++ main.cu
@@ -142,8 +142,8 @@ int main(int argc, char **argv) {
     	real(0.0),
     	thrust::maximum<real>()
     );
-
     real C_lo = -maxF, C_hi = maxF, C = 0.0;
+    //real C_lo = -50, C_hi = 50, C = 0.0;
 
     for (int it=0; it<40; it++) {
         C = 0.5*(C_lo + C_hi);
